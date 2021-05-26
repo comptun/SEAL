d6ff7d419168648b2bbd5f019be4f20cbac31037
Jack Compton <jackedwardcompton@gmail.com>
2021-05-27 00:03:48 +0100

    Added strings + bools + more inputs

--- seal/main.cpp
+++ seal/main.cpp
@@ -20,9 +20,9 @@ std::string fileName;
 std::vector<int> intReg;
 std::vector<float> floatReg;
 std::vector<bool> boolReg;
-std::vector<std::string> strReg;
+std::vector<std::string> stringReg;
 
-bool isNum(std::string a) {
+bool isNum(std::string const& a) {
 	int checkDigit = 0;
 	bool allowedPeriod = true;
 	for (int i = 0; i < a.length(); ++i) {
@@ -35,6 +35,15 @@ bool isNum(std::string a) {
 		}
 	}
 	if (checkDigit == a.length()) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+bool to_bool(std::string const& a) {
+	if (a == "true") {
 		return true;
 	}
 	else {
@@ -82,6 +91,20 @@ int main() {
 								floatReg.at(v) = float(input);
 							}
 						}
+						for (int v = 0; v < bool_.ref.size(); ++v) {
+							if (bool_.ref[v] == varName) {
+								bool input = false;
+								std::cin >> input;
+								boolReg.at(v) = input;
+							}
+						}
+						for (int v = 0; v < string_.ref.size(); ++v) {
+							if (string_.ref[v] == varName) {
+								std::string input;
+								std::cin >> input;
+								stringReg.at(v) = input;
+							}
+						}
 					}
 					break;
 				case _out:
@@ -110,6 +133,16 @@ int main() {
 							for (int v = 0; v < float_.ref.size(); ++v) {
 								if (float_.ref[v] == outStr) {
 									std::cout << floatReg[v];
+								}
+							}
+							for (int v = 0; v < bool_.ref.size(); ++v) {
+								if (bool_.ref[v] == outStr) {
+									std::cout << boolReg[v];
+								}
+							}
+							for (int v = 0; v < string_.ref.size(); ++v) {
+								if (string_.ref[v] == outStr) {
+									std::cout << stringReg[v];
 								}
 							}
 						}
@@ -179,6 +212,70 @@ int main() {
 						}
 					}
 					break;
+				case _bool:
+					if (out[lineCol + 1] == char(32)) {
+						bool cont = true;
+						std::string varName;
+						for (int c = lineCol + 2; c < out.length(); ++c) {
+							if (out[c] == char(59)) {
+								cont = false;
+								boolReg.push_back(false);
+								break;
+							}
+							if (out[c] == char(32)) {
+								break;
+							}
+							varName += out[c];
+							lineCol = c;
+						}
+						bool_.ref.push_back(varName);
+						if (cont) {
+							if (out[lineCol + 2] == char(61)) {
+								lineCol += 4;
+								std::string varVal;
+								for (int c = lineCol; c < out.length(); ++c) {
+									if (out[c] == char(59)) {
+										break;
+									}
+									varVal += out[c];
+								}
+								boolReg.push_back(to_bool(varVal));
+							}
+						}
+					}
+					break;
+				case _string:
+					if (out[lineCol + 1] == char(32)) {
+						bool cont = true;
+						std::string varName;
+						for (int c = lineCol + 2; c < out.length(); ++c) {
+							if (out[c] == char(59)) {
+								cont = false;
+								stringReg.push_back(std::to_string(0));
+								break;
+							}
+							if (out[c] == char(32)) {
+								break;
+							}
+							varName += out[c];
+							lineCol = c;
+						}
+						string_.ref.push_back(varName);
+						if (cont) {
+							if (out[lineCol + 2] == char(61) && out[lineCol + 4] == char(34)) {
+								lineCol += 5;
+								std::string varVal;
+								for (int c = lineCol; c < out.length(); ++c) {
+									if (out[c + 1] == char(59) && out[c] == char(34)) {
+										break;
+									}
+									varVal += out[c];
+								}
+								stringReg.push_back(varVal);
+							}
+						}
+					}
+					break;
 				case _endline:
 					std::cout << "\n";
 					break;
@@ -188,5 +285,7 @@ int main() {
 			}
 		}
 	}
+	int a;
+	std::cin >> a;
 	src.close();
 }
