05aa5c9bf1942bda3942aaf8a7070e1f9874aa78
Jack Compton <jackedwardcompton@gmail.com>
2021-05-26 18:56:46 +0100

    Added floating point data type

--- seal/main.cpp
+++ seal/main.cpp
@@ -9,9 +9,9 @@ enum type {
 	_in,_out,_int,_float,_bool,_string,_endline,_comment
 };
 
-struct vectorSize {
-	int vecSize;
-} _int_,_float_,_bool_,_string_;
+struct ref {
+	std::vector<std::string> ref;
+} int_,float_,bool_,string_;
 
 std::string tokens[TOKENS] = { "in","out","int","float","bool","string","endline","//" };
 std::string out;
@@ -21,11 +21,6 @@ std::vector<int> intReg;
 std::vector<float> floatReg;
 std::vector<bool> boolReg;
 std::vector<std::string> strReg;
-
-std::vector<std::string> intRef;
-std::vector<std::string> floatRef;
-std::vector<std::string> boolRef;
-std::vector<std::string> strRef;
 
 int main() {
 	std::cout << "Enter the name of the .SEAL file you would like to run: ";
@@ -61,28 +56,68 @@ int main() {
 					break;
 				case _int:
 					if (out[lineCol + 1] == char(32)) {
+						bool cont = true;
 						std::string varName;
 						for (int c = lineCol + 2; c < out.length(); ++c) {
+							if (out[c] == char(59)) {
+								cont = false;
+								intReg.push_back(0);
+								break;
+							}
 							if (out[c] == char(32)) {
 								break;
 							}
 							varName += out[c];
 							lineCol = c;
 						}
-						intRef.push_back(varName);
-						if (out[lineCol+2] == char(61)) {
-							lineCol += 4;
-							std::string varVal;
-							for (int c = lineCol; c < out.length(); ++c) {
-								if (out[c] == char(59)) {
-									break;
+						int_.ref.push_back(varName);
+						if (cont) {
+							if (out[lineCol + 2] == char(61)) {
+								lineCol += 4;
+								std::string varVal;
+								for (int c = lineCol; c < out.length(); ++c) {
+									if (out[c] == char(59)) {
+										break;
+									}
+									varVal += out[c];
 								}
-								varVal += out[c];
+								intReg.push_back(std::stoi(varVal));
 							}
-							intReg.push_back(std::stoi(varVal));
 						}
-						++_int_.vecSize;
 						std::cout << intReg[0];
+					}
+					break;
+				case _float:
+					if (out[lineCol + 1] == char(32)) {
+						bool cont = true;
+						std::string varName;
+						for (int c = lineCol + 2; c < out.length(); ++c) {
+							if (out[c] == char(59)) {
+								cont = false;
+								floatReg.push_back(0);
+								break;
+							}
+							if (out[c] == char(32)) {
+								break;
+							}
+							varName += out[c];
+							lineCol = c;
+						}
+						float_.ref.push_back(varName);
+						if (cont) {
+							if (out[lineCol + 2] == char(61)) {
+								lineCol += 4;
+								std::string varVal;
+								for (int c = lineCol; c < out.length(); ++c) {
+									if (out[c] == char(59)) {
+										break;
+									}
+									varVal += out[c];
+								}
+								floatReg.push_back(std::stof(varVal));
+							}
+						}
+						std::cout << floatReg[0];
 					}
 					break;
 				case _endline:
